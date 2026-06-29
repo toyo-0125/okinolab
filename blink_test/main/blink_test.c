03b0c9ef003d8a2e0bbf4fc0f770c87259059195
toyo-0125 <nozomu.6.0125@gmail.com>
2026-06-29 23:25:22 +0900

    Initial ESP32 development environment

---
+++ blink_test/main/blink_test.c
@@ -0,0 +1,19 @@
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/gpio.h"
+
+#define LED_GPIO GPIO_NUM_2
+
+void app_main(void)
+{
+    gpio_reset_pin(LED_GPIO);
+    gpio_set_direction(LED_GPIO, GPIO_MODE_OUTPUT);
+
+    while (1) {
+        gpio_set_level(LED_GPIO, 1);
+        vTaskDelay(pdMS_TO_TICKS(1000));
+
+        gpio_set_level(LED_GPIO, 0);
+        vTaskDelay(pdMS_TO_TICKS(1000));
+    }
+}
